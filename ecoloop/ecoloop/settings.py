16921de4893092c29fb8dad61f936df7f6eaacd6
Garyfimo <garyfimo@gmail.com>
2015-03-02 16:32:47 -0500

    some changes

--- ecoloop/ecoloop/settings.py
+++ ecoloop/ecoloop/settings.py
@@ -81,4 +81,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
+
+
 STATIC_URL = '/static/'
