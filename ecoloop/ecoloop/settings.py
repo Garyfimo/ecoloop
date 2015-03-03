9af36ac5f272945ffad21fcbb27ae11f099549a3
Garyfimo <garyfimo@gmail.com>
2015-03-03 13:01:23 -0500

    some changes for heroku

--- ecoloop/ecoloop/settings.py
+++ ecoloop/ecoloop/settings.py
@@ -92,4 +92,7 @@ STATICFILES_DIRS = (
 )
 
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
