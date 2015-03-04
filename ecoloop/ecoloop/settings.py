879fdb964679edb5b645482bac69e2b6c1eb6944
Garyfimo <garyfimo@gmail.com>
2015-03-03 19:29:22 -0500

    pleaseee god

--- ecoloop/ecoloop/settings.py
+++ ecoloop/ecoloop/settings.py
@@ -60,12 +60,12 @@ WSGI_APPLICATION = 'ecoloop.wsgi.application'
 DATABASES = {
         'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ecoloop',                      # Or path to database file if using sqlite3.
+        'NAME': 'deo75d2d42pqas',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'garyfimo',
+        'USER': 'acpvettjgngqur',
         'PASSWORD': 'morade',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
+        'HOST': 'ec2-50-17-181-147.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '9s7osD4Ps4GtZcqNX4dIUvW4OP',                      # Set to empty string for default.
         }
     }
 
