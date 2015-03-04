1f6f98ad3f94fe98965f911be07d68a92997ab68
Garyfimo <garyfimo@gmail.com>
2015-03-03 19:51:45 -0500

    dfgklmdfldfg

--- ecoloop/ecoloop/settings.py
+++ ecoloop/ecoloop/settings.py
@@ -57,17 +57,17 @@ WSGI_APPLICATION = 'ecoloop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'deo75d2d42pqas',                      # Or path to database file if using sqlite3.
+#DATABASES = {
+#        'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'deo75d2d42pqas',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'acpvettjgngqur',
-        'PASSWORD': '9s7osD4Ps4GtZcqNX4dIUvW4OP',
-        'HOST': 'ec2-50-17-181-147.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
-        }
-    }
+#        'USER': 'acpvettjgngqur',
+#        'PASSWORD': '9s7osD4Ps4GtZcqNX4dIUvW4OP',
+#        'HOST': 'ec2-50-17-181-147.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+#        'PORT': '5432',                      # Set to empty string for default.
+#        }
+#    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -97,5 +97,15 @@ TEMPLATE_DIRS = (
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 
