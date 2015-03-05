e2e1cc69d6ac75312d260fb99eec33e56692bb19
Garyfimo <garyfimo@gmail.com>
2015-03-05 12:23:56 -0500

    some changes in djago admin

--- ecoloop/ecoloop/clothes/admin.py
+++ ecoloop/ecoloop/clothes/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 from .models import Prenda
 # Register your models here.
 
 @admin.register(Prenda)
 class PrendaAdmin(admin.ModelAdmin):
 	pass
+
+class EcoloopAdminSite(AdminSite):
+	site_header = "Ecoloop Administration"
+	site_title = "Administration - Ecoloop"
+
+admin_site = EcoloopAdminSite()
