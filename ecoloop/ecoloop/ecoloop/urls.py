52d3cca393cd979182fb800e96b357f2135a5098
Garyfimo <garyfimo@gmail.com>
2015-03-05 12:33:00 -0500

    some changes in djago admin

--- ecoloop/ecoloop/ecoloop/urls.py
+++ ecoloop/ecoloop/ecoloop/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     url(r'^$', 'clothes.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin/', include(admin_site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin_site.urls)),
 )
