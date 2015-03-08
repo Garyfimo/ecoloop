bc707d7502042154c80521c50e391b49a86d0ec3
Garyfimo <garyfimo@gmail.com>
2015-03-08 11:47:00 -0500

    nothing

--- ecoloop/ecoloop/clothes/views.py
+++ ecoloop/ecoloop/clothes/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+
 
 from .models import Prenda
 
 # Create your views here.
 def home(request):
 	list_prenda = Prenda.objects.all()
-	template = loader.get_template('ecoloop/index.html')
-	context = RequestContext(request, {
+	context = {
 		'list_prenda' : list_prenda,
-		})
-	#return render(request,'index.html')
-	return HttpResponse(template.render(context))
+		}
+	return render(request,'ecoloop/index.html',context)
